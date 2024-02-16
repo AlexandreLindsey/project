b124b5ca232fa851e4ec7673bdc0b716d0dc406f
Alexandre Lindsey <joueur.galactic@gmail.com>
2024-02-16 16:23:20 +0100

    Create odefunction.py

---
+++ odefunction.py
@@ -0,0 +1,45 @@
+"""FILE_DESCRIPTION
+
+Credits
+=======
+Created on Fri Feb 16 15:55:14 2024
+
+@author: Lindsey Alexandre S2302371
+@credits: Luca Odding S2303933, Raffaele Moreci S2304531
+"""
+
+# % [0] Imports
+# Standard library imports.
+import math
+
+# Third-party librairy imports.
+import numpy as np
+import scipy as sp
+import matplotlib as mpl
+from matplotlib import pyplot as plt
+
+# Local module imports
+
+# Global variables
+MM = {'CaO': 56}
+
+
+# % [1] Main Code
+def odefunction(C, z):
+    global MM
+    dC = np.zeros(8)
+
+    dC[0] = MM['CaO']
+    dC[1]
+    dC[2]
+    dC[3]
+    dC[4]
+    dC[5]
+    dC[6]
+    dC[7]
+
+    return dC
+    
+
+
+# % [2] Testing Code
