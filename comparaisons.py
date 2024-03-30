30c1cee6e78279d6841c2eedff2b100bb9227e5d
Alexandre Lindsey <159949119+AlexandreLindsey@users.noreply.github.com>
2024-03-30 20:42:11 +0100

    Update comparaisons.py
    Added more 3D graph (some are weird and need fixing)

--- comparaisons.py
+++ comparaisons.py
@@ -30,6 +30,17 @@ def round_(x, n=0):
 if __name__ == '__main__':
     C0 = [1/4/22.4, 3/4/22.4, 1e-5, 0, 0, 0, c.TW(), 3]
     x_int = [0, c.dim('l')]
+    PARAM2 = np.array([0.0005, 0.001, 0.005, 0.01, 0.02, 0.025])
+    PARAM3 = np.array([0.5, 1, 1.5, 3])
+    C_ = np.array([[1/4/22.4, 3/4/22.4, 1e-5, 0, 0, 0, c.TW(), 3],
+                   [1/3/22.4, 2/3/22.4, 1e-5, 0, 0, 0, c.TW(), 3],
+                   [1/2/22.4, 1/2/22.4, 1e-5, 0, 0, 0, c.TW(), 3],
+                   [2/3/22.4, 1/3/22.4, 1e-5, 0, 0, 0, c.TW(), 3],
+                   [3/4/22.4, 1/4/22.4, 1e-5, 0, 0, 0, c.TW(), 3]])
+    # step = 1e-4
+    # arr_size = 2911
+    step = 1e-5
+    arr_size = 29011
 
     print('Ode mode 0:', end=' ')
     sol = solve_ivp(lambda x, y: ode(x, y, 0), x_int, C0, rtol=0.5e-6)
@@ -54,6 +65,8 @@ if __name__ == '__main__':
     plt.ylabel('Temperature (K)')
     plt.legend(loc='best')
 
+    plt.show()
+
     print('Ode mode 1:', end=' ')
     sol = solve_ivp(lambda x, y: ode(x, y, 1), x_int, C0, rtol=0.5e-6)
     print('completed.')
@@ -75,8 +88,9 @@ if __name__ == '__main__':
     plt.ylabel('Temperature (K)')
     plt.legend(loc='best')
 
-    PARAM = np.array([0.0005, 0.001, 0.005, 0.01, 0.02, 0.025])
-    for i in PARAM:
+    plt.show()
+
+    for i in PARAM2:
         print('Ode mode 2, param ' + str(i) + ':', end=' ')
         sol = solve_ivp(lambda x, y: ode(x, y, 2, i), x_int, C0, rtol=0.5e-6)
         print('completed.')
@@ -128,100 +142,9 @@ if __name__ == '__main__':
     plt.ylabel('Temperature (K)')
     plt.legend(loc='best')
 
-    # Number of outputs:
-    # Step: 1e-4, outputs: 2909
-    # Step: 1e-5, outputs: 29007
-    X = np.zeros((PARAM.size, 29007))
-    Y0 = np.zeros((PARAM.size, 29007))
-    Y1 = np.zeros((PARAM.size, 29007))
-    Y2 = np.zeros((PARAM.size, 29007))
-    Y3 = np.zeros((PARAM.size, 29007))
-    Y4 = np.zeros((PARAM.size, 29007))
-    Y6 = np.zeros((PARAM.size, 29007))
-    Z = np.zeros((PARAM.size, 29007))
-    for i in range(PARAM.size):
-        print('3D Ode mode 2, param ' + str(PARAM[i]) + ':', end=' ')
-        sol = solve_ivp(lambda x, y: ode(x, y, 2, PARAM[i]), x_int, C0,
-                        max_step=1e-5, min_step=1e-5)
-        print('completed.')
-        print(sol.t.size)
-
-        X[i] = sol.t
-        Y0[i] = sol.y[0]
-        Y1[i] = sol.y[1]
-        Y2[i] = sol.y[2]
-        Y3[i] = sol.y[3]
-        Y4[i] = sol.y[4]
-        Y6[i] = sol.y[6]
-        Z[i] = np.full(29007, PARAM[i])
-
-    xticks = np.arange(0, np.max(X)+0.05, 0.05)
-    y0ticks = np.arange(0.002, np.max(Y0)+0.002, 0.002)
-    y1ticks = np.arange(0.02, np.max(Y1)+0.0028, 0.0028)
-    y2ticks = np.arange(0, np.max(Y2)+0.005, 0.005)
-    y3ticks = np.arange(0, np.max(Y3)+0.0006, 0.0006)
-    y4ticks = np.arange(0, np.max(Y4)+0.0008, 0.0008)
-    y6ticks = np.arange(850, np.max(Y6)+24, 24)
-    zticks = np.arange(0, np.max(Z)+0.005, 0.005)
-    empty_labels_x = ["" for i in range(len(xticks))]
-    empty_labels_y0 = ["" for i in range(len(y0ticks))]
-    empty_labels_y1 = ["" for i in range(len(y1ticks))]
-    empty_labels_y2 = ["" for i in range(len(y2ticks))]
-    empty_labels_y3 = ["" for i in range(len(y3ticks))]
-    empty_labels_y4 = ["" for i in range(len(y4ticks))]
-    empty_labels_y6 = ["" for i in range(len(y6ticks))]
-    empty_labels_z = ["" for i in range(len(zticks))]
-
-    fig = plt.figure(23)
-    ax = fig.add_subplot(projection='3d')
-    ax.plot_surface(X, Z, Y0)
-    ax.set_xticks(xticks, empty_labels_x)
-    ax.set_yticks(zticks, empty_labels_z)
-    ax.set_zticks(y0ticks, empty_labels_y0)
     plt.show()
 
-    fig = plt.figure(24)
-    ax = fig.add_subplot(projection='3d')
-    ax.plot_surface(X, Z, Y1)
-    ax.set_xticks(xticks, empty_labels_x)
-    ax.set_yticks(zticks, empty_labels_z)
-    ax.set_zticks(y1ticks, empty_labels_y1)
-    plt.show()
-
-    fig = plt.figure(25)
-    ax = fig.add_subplot(projection='3d')
-    ax.plot_surface(Z, Y2, X)
-    ax.set_xticks(zticks, empty_labels_z)
-    ax.set_yticks(y2ticks, empty_labels_y2)
-    ax.set_zticks(xticks, empty_labels_x)
-    plt.show()
-
-    fig = plt.figure(26)
-    ax = fig.add_subplot(projection='3d')
-    ax.plot_surface(Z, -X, -Y3)
-    ax.set_xticks(zticks, empty_labels_z)
-    ax.set_yticks(-xticks, empty_labels_x)
-    ax.set_zticks(-y3ticks, empty_labels_y3)
-    plt.show()
-
-    fig = plt.figure(27)
-    ax = fig.add_subplot(projection='3d')
-    ax.plot_surface(Z, Y4, X)
-    ax.set_xticks(zticks, empty_labels_z)
-    ax.set_yticks(y4ticks, empty_labels_y4)
-    ax.set_zticks(xticks, empty_labels_x)
-    plt.show()
-
-    fig = plt.figure(28)
-    ax = fig.add_subplot(projection='3d')
-    ax.plot_surface(X, Z, Y6)
-    ax.set_xticks(xticks, empty_labels_x)
-    ax.set_yticks(zticks, empty_labels_z)
-    ax.set_zticks(y6ticks, empty_labels_y6)
-    plt.show()
-
-    PARAM = np.array([0.5, 1, 1.5, 3])
-    for i in PARAM:
+    for i in PARAM3:
         print('Ode mode 3, param ' + str(i) + ':', end=' ')
         sol = solve_ivp(lambda x, y: ode(x, y, 3, i), x_int, C0, rtol=0.5e-6)
         print('completed.')
@@ -273,17 +196,15 @@ if __name__ == '__main__':
     plt.ylabel('Temperature (K)')
     plt.legend(loc='best')
 
-    C_ = [[1/4/22.4, 3/4/22.4, 1e-5, 0, 0, 0, c.TW(), 3],
-          [2/3/22.4, 1/3/22.4, 1e-5, 0, 0, 0, c.TW(), 3],
-          [1/2/22.4, 1/2/22.4, 1e-5, 0, 0, 0, c.TW(), 3],
-          [1/3/22.4, 2/3/22.4, 1e-5, 0, 0, 0, c.TW(), 3],
-          [3/4/22.4, 1/4/22.4, 1e-5, 0, 0, 0, c.TW(), 3]]
-    for C0 in C_:
-        print('Ode with C0 = ' + str(C0) + ':', end=' ')
-        sol = solve_ivp(lambda x, y: ode(x, y, 0), x_int, C0, rtol=0.5e-6)
+    plt.show()
+
+    for C0_ in C_:
+        print('Ode with H2O/CH4 = ' + str(round((C0_[1]/C0_[0])*10)/10)
+              + ':', end=' ')
+        sol = solve_ivp(lambda x, y: ode(x, y, 0), x_int, C0_, rtol=0.5e-6)
         print('completed.')
 
-        legend = 'H20/CH4: ' + str(round((C0[1]/C0[0])*10)/10)
+        legend = 'H20/CH4: ' + str(round((C0_[1]/C0_[0])*10)/10)
         plt.figure(17)
         plt.plot(sol.t, sol.y[0], label=legend, linewidth=1)
         plt.figure(18)
@@ -329,3 +250,285 @@ if __name__ == '__main__':
     plt.xlabel('z (m)')
     plt.ylabel('Temperature (K)')
     plt.legend(loc='best')
+
+    plt.show()
+
+    X = np.zeros((PARAM2.size, arr_size))
+    Y0 = np.zeros((PARAM2.size, arr_size))
+    Y1 = np.zeros((PARAM2.size, arr_size))
+    Y2 = np.zeros((PARAM2.size, arr_size))
+    Y3 = np.zeros((PARAM2.size, arr_size))
+    Y4 = np.zeros((PARAM2.size, arr_size))
+    Y6 = np.zeros((PARAM2.size, arr_size))
+    Z = np.zeros((PARAM2.size, arr_size))
+    for i in range(PARAM2.size):
+        print('3D | Ode mode 2, param ' + str(PARAM2[i]) + ':', end=' ')
+        sol = solve_ivp(lambda x, y: ode(x, y, 2, PARAM2[i]), x_int, C0,
+                        max_step=step, min_step=step)
+        print('completed.')
+
+        arr = np.full(arr_size, sol.t[-1])
+        arr[:sol.t.size] = sol.t
+        X[i] = arr
+        arr = np.full(arr_size, sol.y[0][-1])
+        arr[:sol.y[0].size] = sol.y[0]
+        Y0[i] = arr
+        arr = np.full(arr_size, sol.y[1][-1])
+        arr[:sol.y[1].size] = sol.y[1]
+        Y1[i] = arr
+        arr = np.full(arr_size, sol.y[2][-1])
+        arr[:sol.y[2].size] = sol.y[2]
+        Y2[i] = arr
+        arr = np.full(arr_size, sol.y[3][-1])
+        arr[:sol.y[3].size] = sol.y[3]
+        Y3[i] = arr
+        arr = np.full(arr_size, sol.y[4][-1])
+        arr[:sol.y[4].size] = sol.y[4]
+        Y4[i] = arr
+        arr = np.full(arr_size, sol.y[6][-1])
+        arr[:sol.y[6].size] = sol.y[6]
+        Y6[i] = arr
+        Z[i] = np.full(arr_size, PARAM2[i])
+
+    xticks = np.arange(0, np.max(X)+0.05, 0.05)
+    y0ticks = np.arange(0.002, np.max(Y0)+0.002, 0.002)
+    y1ticks = np.arange(0.02, np.max(Y1)+0.0028, 0.0028)
+    y2ticks = np.arange(0, np.max(Y2)+0.005, 0.005)
+    y3ticks = np.arange(0, np.max(Y3)+0.0006, 0.0006)
+    y4ticks = np.arange(0, np.max(Y4)+0.0008, 0.0008)
+    y6ticks = np.arange(850, np.max(Y6)+24, 24)
+    zticks = np.arange(0, np.max(Z)+0.005, 0.005)
+    empty_labels_x = ["" for i in range(len(xticks))]
+    empty_labels_y0 = ["" for i in range(len(y0ticks))]
+    empty_labels_y1 = ["" for i in range(len(y1ticks))]
+    empty_labels_y2 = ["" for i in range(len(y2ticks))]
+    empty_labels_y3 = ["" for i in range(len(y3ticks))]
+    empty_labels_y4 = ["" for i in range(len(y4ticks))]
+    empty_labels_y6 = ["" for i in range(len(y6ticks))]
+    empty_labels_z = ["" for i in range(len(zticks))]
+
+    fig = plt.figure(23)
+    ax = fig.add_subplot(projection='3d')
+    ax.plot_surface(X, Z, Y0)
+    ax.set_xticks(xticks, empty_labels_x)
+    ax.set_yticks(zticks, empty_labels_z)
+    ax.set_zticks(y0ticks, empty_labels_y0)
+    fig = plt.figure(24)
+    ax = fig.add_subplot(projection='3d')
+    ax.plot_surface(X, Z, Y1)
+    ax.set_xticks(xticks, empty_labels_x)
+    ax.set_yticks(zticks, empty_labels_z)
+    ax.set_zticks(y1ticks, empty_labels_y1)
+    fig = plt.figure(25)
+    ax = fig.add_subplot(projection='3d')
+    ax.plot_surface(Z, Y2, X)
+    ax.set_xticks(zticks, empty_labels_z)
+    ax.set_yticks(y2ticks, empty_labels_y2)
+    ax.set_zticks(xticks, empty_labels_x)
+    fig = plt.figure(26)
+    ax = fig.add_subplot(projection='3d')
+    ax.plot_surface(Z, -X, -Y3)
+    ax.set_xticks(zticks, empty_labels_z)
+    ax.set_yticks(-xticks, empty_labels_x)
+    ax.set_zticks(-y3ticks, empty_labels_y3)
+    fig = plt.figure(27)
+    ax = fig.add_subplot(projection='3d')
+    ax.plot_surface(Z, Y4, X)
+    ax.set_xticks(zticks, empty_labels_z)
+    ax.set_yticks(y4ticks, empty_labels_y4)
+    ax.set_zticks(xticks, empty_labels_x)
+    fig = plt.figure(28)
+    ax = fig.add_subplot(projection='3d')
+    ax.plot_surface(X, Z, Y6)
+    ax.set_xticks(xticks, empty_labels_x)
+    ax.set_yticks(zticks, empty_labels_z)
+    ax.set_zticks(y6ticks, empty_labels_y6)
+
+    plt.show()
+
+    X = np.zeros((PARAM3.size, arr_size))
+    Y0 = np.zeros((PARAM3.size, arr_size))
+    Y1 = np.zeros((PARAM3.size, arr_size))
+    Y2 = np.zeros((PARAM3.size, arr_size))
+    Y3 = np.zeros((PARAM3.size, arr_size))
+    Y4 = np.zeros((PARAM3.size, arr_size))
+    Y6 = np.zeros((PARAM3.size, arr_size))
+    Z = np.zeros((PARAM3.size, arr_size))
+    for i in range(PARAM3.size):
+        print('3D | Ode mode 3, param ' + str(PARAM3[i]) + ':', end=' ')
+        sol = solve_ivp(lambda x, y: ode(x, y, 3, PARAM3[i]), x_int, C0,
+                        max_step=step, min_step=step)
+        print('completed.')
+
+        arr = np.full(arr_size, sol.t[-1])
+        arr[:sol.t.size] = sol.t
+        X[i] = arr
+        arr = np.full(arr_size, sol.y[0][-1])
+        arr[:sol.y[0].size] = sol.y[0]
+        Y0[i] = arr
+        arr = np.full(arr_size, sol.y[1][-1])
+        arr[:sol.y[1].size] = sol.y[1]
+        Y1[i] = arr
+        arr = np.full(arr_size, sol.y[2][-1])
+        arr[:sol.y[2].size] = sol.y[2]
+        Y2[i] = arr
+        arr = np.full(arr_size, sol.y[3][-1])
+        arr[:sol.y[3].size] = sol.y[3]
+        Y3[i] = arr
+        arr = np.full(arr_size, sol.y[4][-1])
+        arr[:sol.y[4].size] = sol.y[4]
+        Y4[i] = arr
+        arr = np.full(arr_size, sol.y[6][-1])
+        arr[:sol.y[6].size] = sol.y[6]
+        Y6[i] = arr
+        Z[i] = np.full(arr_size, PARAM3[i])
+
+    xticks = np.arange(0, np.max(X)+0.05, 0.05)
+    y0ticks = np.arange(0.002, np.max(Y0)+0.002, 0.002)
+    y1ticks = np.arange(0.02, np.max(Y1)+0.0028, 0.0028)
+    y2ticks = np.arange(0, np.max(Y2)+0.005, 0.005)
+    y3ticks = np.arange(0, np.max(Y3)+0.0006, 0.0006)
+    y4ticks = np.arange(0, np.max(Y4)+0.0008, 0.0008)
+    y6ticks = np.arange(850, np.max(Y6)+24, 24)
+    zticks = np.arange(0, np.max(Z)+0.005, 0.005)
+    empty_labels_x = ["" for i in range(len(xticks))]
+    empty_labels_y0 = ["" for i in range(len(y0ticks))]
+    empty_labels_y1 = ["" for i in range(len(y1ticks))]
+    empty_labels_y2 = ["" for i in range(len(y2ticks))]
+    empty_labels_y3 = ["" for i in range(len(y3ticks))]
+    empty_labels_y4 = ["" for i in range(len(y4ticks))]
+    empty_labels_y6 = ["" for i in range(len(y6ticks))]
+    empty_labels_z = ["" for i in range(len(zticks))]
+
+    fig = plt.figure(29)
+    ax = fig.add_subplot(projection='3d')
+    ax.plot_surface(X, Z, Y0)
+    ax.set_xticks(xticks, empty_labels_x)
+    ax.set_yticks(zticks, empty_labels_z)
+    ax.set_zticks(y0ticks, empty_labels_y0)
+    fig = plt.figure(30)
+    ax = fig.add_subplot(projection='3d')
+    ax.plot_surface(X, Z, Y1)
+    ax.set_xticks(xticks, empty_labels_x)
+    ax.set_yticks(zticks, empty_labels_z)
+    ax.set_zticks(y1ticks, empty_labels_y1)
+    fig = plt.figure(31)
+    ax = fig.add_subplot(projection='3d')
+    ax.plot_surface(Z, Y2, X)
+    ax.set_xticks(zticks, empty_labels_z)
+    ax.set_yticks(y2ticks, empty_labels_y2)
+    ax.set_zticks(xticks, empty_labels_x)
+    fig = plt.figure(32)
+    ax = fig.add_subplot(projection='3d')
+    ax.plot_surface(Z, -X, -Y3)
+    ax.set_xticks(zticks, empty_labels_z)
+    ax.set_yticks(-xticks, empty_labels_x)
+    ax.set_zticks(-y3ticks, empty_labels_y3)
+    fig = plt.figure(33)
+    ax = fig.add_subplot(projection='3d')
+    ax.plot_surface(Z, Y4, X)
+    ax.set_xticks(zticks, empty_labels_z)
+    ax.set_yticks(y4ticks, empty_labels_y4)
+    ax.set_zticks(xticks, empty_labels_x)
+    fig = plt.figure(34)
+    ax = fig.add_subplot(projection='3d')
+    ax.plot_surface(X, Z, Y6)
+    ax.set_xticks(xticks, empty_labels_x)
+    ax.set_yticks(zticks, empty_labels_z)
+    ax.set_zticks(y6ticks, empty_labels_y6)
+
+    plt.show()
+
+    X = np.zeros((C_.size, arr_size))
+    Y0 = np.zeros((C_.size, arr_size))
+    Y1 = np.zeros((C_.size, arr_size))
+    Y2 = np.zeros((C_.size, arr_size))
+    Y3 = np.zeros((C_.size, arr_size))
+    Y4 = np.zeros((C_.size, arr_size))
+    Y6 = np.zeros((C_.size, arr_size))
+    Z = np.zeros((C_.size, arr_size))
+    for i in range(C_.shape[0]):
+        print('3D | Ode with H2O/CH4 = '
+              + str(round((C_[i][1]/C_[i][0])*10)/10) + ':', end=' ')
+        sol = solve_ivp(lambda x, y: ode(x, y, 0), x_int, C_[i],
+                        max_step=step, min_step=step)
+        print('completed.')
+
+        arr = np.full(arr_size, sol.t[-1])
+        arr[:sol.t.size] = sol.t
+        X[i] = arr
+        arr = np.full(arr_size, sol.y[0][-1])
+        arr[:sol.y[0].size] = sol.y[0]
+        Y0[i] = arr
+        arr = np.full(arr_size, sol.y[1][-1])
+        arr[:sol.y[1].size] = sol.y[1]
+        Y1[i] = arr
+        arr = np.full(arr_size, sol.y[2][-1])
+        arr[:sol.y[2].size] = sol.y[2]
+        Y2[i] = arr
+        arr = np.full(arr_size, sol.y[3][-1])
+        arr[:sol.y[3].size] = sol.y[3]
+        Y3[i] = arr
+        arr = np.full(arr_size, sol.y[4][-1])
+        arr[:sol.y[4].size] = sol.y[4]
+        Y4[i] = arr
+        arr = np.full(arr_size, sol.y[6][-1])
+        arr[:sol.y[6].size] = sol.y[6]
+        Y6[i] = arr
+        Z[i] = np.full(arr_size, C_[i][1]/C_[i][0])
+
+    xticks = np.arange(0, np.max(X)+0.05, 0.05)
+    y0ticks = np.arange(0, np.max(Y0)+0.007, 0.007)
+    y1ticks = np.arange(0, np.max(Y1)+0.007, 0.007)
+    y2ticks = np.arange(0, np.max(Y2)+0.005, 0.005)
+    y3ticks = np.arange(0, np.max(Y3)+0.0009, 0.0009)
+    y4ticks = np.arange(0, np.max(Y4)+0.0008, 0.0008)
+    y6ticks = np.arange(840, np.max(Y6)+28, 28)
+    zticks = np.arange(0, np.max(Z)+0.005, 0.005)
+    empty_labels_x = ["" for i in range(len(xticks))]
+    empty_labels_y0 = ["" for i in range(len(y0ticks))]
+    empty_labels_y1 = ["" for i in range(len(y1ticks))]
+    empty_labels_y2 = ["" for i in range(len(y2ticks))]
+    empty_labels_y3 = ["" for i in range(len(y3ticks))]
+    empty_labels_y4 = ["" for i in range(len(y4ticks))]
+    empty_labels_y6 = ["" for i in range(len(y6ticks))]
+    empty_labels_z = ["" for i in range(len(zticks))]
+
+    fig = plt.figure(35)
+    ax = fig.add_subplot(projection='3d')
+    ax.plot_surface(X, Z, Y0)
+    ax.set_xticks(xticks, empty_labels_x)
+    ax.set_yticks(zticks, empty_labels_z)
+    ax.set_zticks(y0ticks, empty_labels_y0)
+    fig = plt.figure(36)
+    ax = fig.add_subplot(projection='3d')
+    ax.plot_surface(X, Z, Y1)
+    ax.set_xticks(xticks, empty_labels_x)
+    ax.set_yticks(zticks, empty_labels_z)
+    ax.set_zticks(y1ticks, empty_labels_y1)
+    fig = plt.figure(37)
+    ax = fig.add_subplot(projection='3d')
+    ax.plot_surface(Z, Y2, X)
+    ax.set_xticks(zticks, empty_labels_z)
+    ax.set_yticks(y2ticks, empty_labels_y2)
+    ax.set_zticks(xticks, empty_labels_x)
+    fig = plt.figure(38)
+    ax = fig.add_subplot(projection='3d')
+    ax.plot_surface(Z, -X, -Y3)
+    ax.set_xticks(zticks, empty_labels_z)
+    ax.set_yticks(-xticks, empty_labels_x)
+    ax.set_zticks(-y3ticks, empty_labels_y3)
+    fig = plt.figure(39)
+    ax = fig.add_subplot(projection='3d')
+    ax.plot_surface(Z, Y4, X)
+    ax.set_xticks(zticks, empty_labels_z)
+    ax.set_yticks(y4ticks, empty_labels_y4)
+    ax.set_zticks(xticks, empty_labels_x)
+    fig = plt.figure(40)
+    ax = fig.add_subplot(projection='3d')
+    ax.plot_surface(X, Z, Y6)
+    ax.set_xticks(xticks, empty_labels_x)
+    ax.set_yticks(zticks, empty_labels_z)
+    ax.set_zticks(y6ticks, empty_labels_y6)
+
+    plt.show()
